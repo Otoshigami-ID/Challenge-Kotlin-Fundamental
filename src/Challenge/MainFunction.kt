27948b11b4a69a0b0d8944d596d11efc76c4aeb5
Rifqi Maheswara <rifqimaheswara@gmail.com>
2020-08-29 23:45:29 +0700

    challenge commit

---
+++ src/Challenge/MainFunction.kt
@@ -0,0 +1,44 @@
+package Challenge
+
+import java.util.*
+import Challenge.VBalok.main
+
+fun main() {
+
+    val input = Scanner(System.`in`)
+
+    print("Name : ")
+    input.nextLine()
+
+    print("kelas : ")
+    input.nextLine()
+
+    print("No Absen : ")
+    input.nextInt()
+    println("===========================")
+    println("Kamu Ingin Menghitung apa?")
+    println("1. Volume Balok")
+    println("2. Volume Bola")
+    println("3. Volume Kerucut")
+    println("4. Volume Tabung")
+    println("0. Batal")
+    println("===========================")
+    print("Pilihanmu : ")
+
+    when (input.nextInt()) {
+        1 -> {
+            main()
+        }
+        2 -> {
+            Challenge.VBola.main()
+        }
+        3 -> {
+            Challenge.VKerucut.main()
+        }
+        4 -> {
+            Challenge.VTabung.main()
+        }
+        0 -> {
+        }
+    }
+}
---
+++ src/Challenge/VKerucut/VolumeKerucut.kt
@@ -0,0 +1,22 @@
+package Challenge.VKerucut
+
+import java.util.*
+
+fun main() {
+    var jari: Float
+    var volumepi: Float
+    val tinggi:Double
+
+    val reader = Scanner(System.`in`)
+
+    print("Masukkan Jari jari : ")
+    jari = reader.nextFloat()
+
+    print("Masukkan Tinggi : ")
+    tinggi = reader.nextDouble()
+
+    volumepi = (((jari * jari * 22 / 7 * tinggi) / 3).toFloat())
+    val s = String.format("%.2f", volumepi)
+    println("===========================")
+    print("Jadi Volume Kerucut = $s")
+}
